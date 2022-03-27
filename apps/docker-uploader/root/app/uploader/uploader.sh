82651f148e9bc2a4f81cbe170c207cd60d67eed7
doob187 <60312740+doob187@users.noreply.github.com>
2022-03-27 15:13:31 +0200

    Update uploader.sh

--- apps/docker-uploader/root/app/uploader/uploader.sh
+++ apps/docker-uploader/root/app/uploader/uploader.sh
@@ -19,6 +19,7 @@ log "dockserver.io Multi-Thread Uploader started"
 
 BASE=/system/uploader
 CONFIG=/system/servicekeys/rclonegdsa.conf
+CSV=/system/servicekeys/uploader.csv
 KEYLOCAL=/system/servicekeys/keys/
 LOGFILE=/system/uploader/logs
 START=/system/uploader/json/upload
@@ -40,17 +41,66 @@ find "${BASE}" -type f -name '*.log' -delete
 find "${BASE}" -type f -name '*.txt' -delete
 find "${START}" -type f -name '*.json' -delete
 
-if `rclone config show --config=${CONFIG} | grep ":/encrypt" &>/dev/null`;then
-   export CRYPTED=C
+
+if test -f ${CONFIG}; then
+   if `rclone config show --config=${CONFIG} | grep ":/encrypt" &>/dev/null`;then
+      export CRYPTED=C
+   fi
+   if `rclone config show --config=${CONFIG} | grep "GDSA" &>/dev/null`;then
+       export KEY=GDSA
+   elif `rclone config show --config=${CONFIG} | head -n1 | grep -Po '\[.*?]' | sed 's/.*\[\([^]]*\)].*/\1/' | sed '/GDSA/d'`;then
+       export KEY=""
+   else
+       log "no match found of GDSA[01=~100] or [01=~100]"
+       sleep infinity
+   fi
 fi
-if `rclone config show --config=${CONFIG} | grep "GDSA" &>/dev/null`;then
-   export KEY=GDSA
-elif `rclone config show --config=${CONFIG} | head -n1 | grep -Po '\[.*?]' | sed 's/.*\[\([^]]*\)].*/\1/' | sed '/GDSA/d'`;then
-   export KEY=""
+
+function loopcsv() {
+CSV=/system/servicekeys/uploader.csv
+if test -f ${CSV} ; then
+   UPP=${UPP}
+   MOVE=${MOVE:-/}
+   FILE=$(basename "${UPP[1]}")
+   DIR=$(dirname "${UPP[1]}" | sed "s#${DLFOLDER}/${MOVE}##g")
+   ENDCONFIG=/system/servicekeys/${UPP}.conf
+   ARRAY=$(ls -A ${KEYLOCAL} | wc -l )
+   USED=$(( $RANDOM % ${ARRAY} + 1 ))
+
+   cat ${CSV} | grep -E ${DIR} | sed '/^\s*#.*$/d'| while IFS=$'|' read -ra myArray; do
+   if [[ ${myArray[2]} == "" && ${myArray[3]} == "" ]]; then
+   echo -e "\n
+[${USED[0]}]
+type = drive
+scope = drive
+server_side_across_configs = true
+service_account_file = ${JSONDIR}/${USED}
+team_drive = ${myArray[1]}" >>$ENDCONFIG
+
 else
-   log "no match found of GDSA[01=~100] or [01=~100]"
-   sleep infinity
+
+ENC_PASSWORD=${myArray[2]}
+ENC_SALT=${myArray[3]}
+   echo -e "\n
+[${USED[0]}]
+type = drive
+scope = drive
+server_side_across_configs = true
+service_account_file = ${JSONDIR}/${USED}
+team_drive = ${myArray[1]}
+##
+type = crypt
+remote = ${USED[0]}:/encrypt
+filename_encryption = standard
+directory_name_encryption = true
+password = ${ENC_PASSWORD}
+password2 = ${ENC_SALT}" >>$ENDCONFIG
+   fi
+done
 fi
+
+}
+
 if [[ ! -f ${EXCLUDE} ]]; then
    cat > ${EXCLUDE} << EOF; $(echo)
 *-vpn/**
@@ -70,21 +120,24 @@ EOF
 fi
 
 function rcloneupload() {
+
 UPP=${UPP}
 MOVE=${MOVE:-/}
+DLFOLDER=${DLFOLDER}
 FILE=$(basename "${UPP[1]}")
 DIR=$(dirname "${UPP[1]}" | sed "s#${DLFOLDER}/${MOVE}##g")
 STARTZ=$(date +%s)
 SIZE=$(stat -c %s "${DLFOLDER}/${UPP[1]}" | numfmt --to=iec-i --suffix=B --padding=7)
+
 while true; do
   SUMSTART=$(stat -c %s "${DLFOLDER}/${UPP[1]}")
   sleep 5
   SUMTEST=$(stat -c %s "${DLFOLDER}/${UPP[1]}")
-if [[ "$SUMSTART" -eq "$SUMTEST" ]]; then
-   sleep 1 && break
-else
-   sleep 1 && continue
-fi
+  if [[ "$SUMSTART" -eq "$SUMTEST" ]]; then
+     sleep 1 && break
+  else
+     sleep 1 && continue
+  fi
 done
 
 UPFILE=$($(which rclone) size "${DLFOLDER}/${UPP[1]}" --config="${CONFIG}" --json | cut -d ":" -f3 | cut -d "}" -f1)
@@ -92,6 +145,12 @@ touch "${LOGFILE}/${FILE}.txt"
 ARRAY=$(ls -A ${KEYLOCAL} | wc -l )
 USED=$(( $RANDOM % ${ARRAY} + 1 ))
   echo "{\"filedir\": \"${DIR}\",\"filebase\": \"${FILE}\",\"filesize\": \"${SIZE}\",\"logfile\": \"${LOGFILE}/${FILE}.txt\",\"gdsa\": \"${KEY}$[USED]${CRYPTED}\"}" > "${START}/${FILE}.json"
+
+if test -f "/app/rclone/${UPP}.conf";then
+   CONFIG=/system/servicekeys/${UPP}.conf
+else
+   CONFIG=/system/servicekeys/rclonegdsa.conf
+fi
 
 ## RUN MOVE
 $(which rclone) move "${DLFOLDER}/${UPP[1]}" "${KEY}$[USED]${CRYPTED}:/${DIR}/" --config="${CONFIG}" \
@@ -104,6 +163,11 @@ echo "{\"filedir\": \"${DIR}\",\"filebase\": \"${FILE}\",\"filesize\": \"${SIZE}
 ## END OF MOVE
 $(which rm) -rf "${LOGFILE}/${FILE}.txt" "${START}/${FILE}.json" 
 $(which chmod) 755 "${DONE}/${FILE}.json"
+
+if test -f "/app/rclone/${UPP}.conf";then
+   $(which rm) -rf /system/servicekeys/${UPP}.conf
+fi
+
 }
 
 ## START HERE UPLOADER LIVE
@@ -138,6 +202,10 @@ while true; do
               sleep 10 && continue
            fi
          done
+         ## Looping to correct drive
+         if test -f ${CSV} ; then
+           loopcsv
+         fi
          ## upload function startup
          rcloneupload
          ## upload function shutdown
