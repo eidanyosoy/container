e6f894984fc4ffe4f3d5fbb4942b257ae5aa834a
doob187 <60312740+doob187@users.noreply.github.com>
2022-04-01 19:39:16 +0200

    Update meta.sh

--- base/docker-config/root/meta.sh
+++ base/docker-config/root/meta.sh
@@ -4,22 +4,33 @@
 
 for i in $(ls -d /opt/appdata/*/); do basename -- ${i%%/}; done
 ## Zeigt die arrs alle an
+function arrs() {
+for i in $(ls -d /apps/*/ | grep '*arr"'); do
 
-    sonarr=$(curl -sX GET http://sonarr:8989/api/rootfolder/1?apikey= apikey -H "accept: application/json" | jq -r '.|.path')
-    sonarr4k=$(curl -sX GET http://sonarr4k:8989/api/rootfolder/1?apikey= apkiey -H "accept: application/json" | jq -r '.|.path')
-    radarr=$(curl -sX GET http://radarr:7878/api/v3/rootfolder/1?apikey= apikey -H "accept: application/json" | jq -r '.|.path')
-    radarr4k=$(curl -sX GET http://radarr4k:7878/api/v3/rootfolder/1?apikey= apikey -H "accept: application/json" | jq -r '.|.path')
+    APP=$(basename -- ${APP%%/})
+    KEY=$(cat /apps/$i/config.xml) 1>/dev/null 2>&1
+    KEY=${KEY#*<ApiKey>} 1>/dev/null 2>&1
+    
+    if [ $APP == "sonarr" ]; then
+       PORT=8989
+    elif [ $APP == "radarr" ]; then
+       PORT=7878
+    elif [ $APP == "lidarr" ]; then
+       PORT=8686
+    elif [ $APP == "bazarr" ]; then
+       PORT=6767
+    elif [ $APP == "readarr" ]; then
+       PORT=8787
+    else
+       echo "can't find any arr running "
+    fi
 
-echo "sonarr = $sonarr"
-echo "sonarr4k = $sonarr4k"
-echo "radarr = $radarr"
-echo "radarr4k = $radarr4k"
+    APIKEYECHO=$(curl -sX GET http://${APP}:${PORT}/api/rootfolder/1?apikey=$(echo ${KEY:0:32}) -H "accept: application/json" | jq -r '.|.path')
 
+    echo "${APP} = ${APIKEYECHO} = ${KEY:0:32}" >> /config/arrkeys
+
+done
+}
 
 PLEX_TOKEN=$(cat "/opt/appdata/plex/database/Library/Application Support/Plex Media Server/Preferences.xml" | sed -e 's;^.* PlexOnlineToken=";;' | sed -e 's;".*$;;' | tail -1)
 
-
-## get apkiey für die arrs
-info=$(cat /opt/appdata/radarr/config.xml)
-info=${info#*<ApiKey>} 1>/dev/null 2>&1
-info1=$(echo ${info:0:32})
