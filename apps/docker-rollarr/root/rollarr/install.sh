8bba8c0a340d4756b07a57746937037d19b84686
doob187 <60312740+doob187@users.noreply.github.com>
2022-04-09 18:34:54 +0200

    Update install.sh

--- apps/docker-rollarr/root/rollarr/install.sh
+++ apps/docker-rollarr/root/rollarr/install.sh
@@ -8,32 +8,30 @@ PGID="1000"
 DEBIAN_FRONTEND="noninteractive"
 GPG_KEY=A035C8C19219BA821ECEA86B64E628F8D684696D
 PYTHON_VERSION=3.10.1
-PYTHON_PIP_VERSION=21.2.4
-PYTHON_SETUPTOOLS_VERSION=57.5.0
-PYTHON_GET_PIP_URL=https://github.com/pypa/get-pip/raw/3cb8888cc2869620f57d5d2da64da38f516078c7/public/get-pip.py
-PYTHON_GET_PIP_SHA256=c518250e91a70d7b20cceb15272209a4ded2a0c263ae5776f129e0d9b5674309
+PYTHON_PIP_VERSION=22.0.4
+PYTHON_SETUPTOOLS_VERSION=62.0.0
 PATH=/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 set -eux && \
    apt-get update && \
-   apt-get install -y --no-install-recommends ca-certificates curl netbase wget;
-rm -rf /var/lib/apt/lists/*
+   apt-get install -yqq --no-install-recommends ca-certificates curl netbase wget && \
+   rm -rf /var/lib/apt/lists/*
 
-set -ex && \
+set -eux && \
    if ! command -v gpg > /dev/null; then
-      apt-get update && \
-      apt-get install -y --no-install-recommends gnupg dirmngr && \
+      apt-get update -yqq && \
+      apt-get install -yqq --no-install-recommends gnupg dirmngr && \
       rm -rf /var/lib/apt/lists/*
    fi
 
-set -ex && \
-   apt-get update && \
-   apt-get install -y --no-install-recommends git mercurial openssh-client subversion procps && \
+set -eux && \
+   apt-get update -yqq && \
+   apt-get install -yqq --no-install-recommends git mercurial openssh-client subversion procps && \
    rm -rf /var/lib/apt/lists/*
 
-set -ex && \
-   apt-get update && \
-   apt-get install -y --no-install-recommends \
+set -eux && \
+   apt-get update -yqq && \
+   apt-get install -yqq --no-install-recommends \
      autoconf \
      automake \
      bzip2 \
@@ -75,12 +73,12 @@ set -ex && \
      xz-utils \
      zlib1g-dev \
      $( if apt-cache show 'default-libmysqlclient-dev' 2>/dev/null | grep -q '^Version:'; then echo 'default-libmysqlclient-dev'; else echo 'libmysqlclient-dev'; fi ) && \
- rm -rf /var/lib/apt/lists/*
+     rm -rf /var/lib/apt/lists/*
 
 set -ex && \
-   apt-get update && \
-   apt-get install -y --no-install-recommends libbluetooth-dev tk-dev uuid-dev && \
- rm -rf /var/lib/apt/lists/*
+   apt-get update -yqq && \
+   apt-get install -yqq --no-install-recommends libbluetooth-dev tk-dev uuid-dev && \
+   rm -rf /var/lib/apt/lists/*
 
 set -ex && \
    wget -O python.tar.xz "https://www.python.org/ftp/python/${PYTHON_VERSION%%[a-z]*}/Python-$PYTHON_VERSION.tar.xz" && \
@@ -92,7 +90,7 @@ set -ex && \
      rm -rf "$GNUPGHOME" python.tar.xz.asc && \
      mkdir -p /usr/src/python && \
      tar -xJC /usr/src/python --strip-components=1 -f python.tar.xz && \
-     rm python.tar.xz && \
+     rm -rf python.tar.xz && \
      cd /usr/src/python && \
        gnuArch="$(dpkg-architecture --query DEB_BUILD_GNU_TYPE)" && \
        ./configure \
@@ -118,13 +116,23 @@ cd /usr/local/bin && \
    ln -s python3-config python-config
 
 set -ex && \
-   wget -O get-pip.py "$PYTHON_GET_PIP_URL" && \
-   echo "$PYTHON_GET_PIP_SHA256 *get-pip.py" | sha256sum --check --strict - && \
-   python get-pip.py --disable-pip-version-check --no-cache-dir "pip==$PYTHON_PIP_VERSION" "setuptools==$PYTHON_SETUPTOOLS_VERSION" && \
-   pip --version && \
+   curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py && \
+      python get-pip.py --disable-pip-version-check --no-cache-dir "pip==$PYTHON_PIP_VERSION" "setuptools==$PYTHON_SETUPTOOLS_VERSION" && \
+      pip --version && \
    find /usr/local -depth \( \( -type d -a \( -name test -o -name tests -o -name idle_test \) \) -o \( -type f -a \( -name '*.pyc' -o -name '*.pyo' \) \) \) -exec rm -rf '{}' + && \
-   rm -f get-pip.py
+      rm -rf get-pip.py && \
+      python3 -m pip install --upgrade pip && \
+      python3 -m pip install --no-warn-script-location --upgrade --python-version="${PYTHON_VERSION}" --force-reinstall -r /rollarr/requirements.txt && \
+   rm -rf /tmp/* && \
+   rm -rf /var/lib/apt/lists/* && \
+   rm -rf /root/.cache
 
+echo -e "#run python script every hour
+0 * * * * /usr/local/bin/python /rollarr/PrerollUpdate.py > /proc/1/fd/1 2>/proc/1/fd/2
+#empty" >/crontab.conf
 
+/usr/bin/crontab /crontab.conf
 
+mkdir -p /config && \
+ln -s /rollarr/data.json /config/data.json
 
