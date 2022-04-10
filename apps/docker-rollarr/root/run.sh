e99319061b9258d365e0d3ced2ef0351530b6f62
doob187 <60312740+doob187@users.noreply.github.com>
2022-04-10 03:32:28 +0200

    Update run.sh

--- apps/docker-rollarr/root/run.sh
+++ apps/docker-rollarr/root/run.sh
@@ -1,3 +1,16 @@
-## run as user
+#!/bin/bash
+
+if [[ ! -f "/config/config.json" ]]; then
+   cp /rollarr/data_sample.json /config/data.json
+fi
+
+chmod -R 755 /rollarr/* \  
+          /config/data.json \
+          /crontab &>/dev/null
+
+chown -cR abc:abc /rollarr/* \
+         /config/data.json \
+         /crontab &>/dev/null
+
 exec cron -f & \
 exec python /rollarr/Preroll.py
