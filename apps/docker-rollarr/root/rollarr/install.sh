0a7570b9d1eba3ce428ee9460d9358c9d064a9bc
doob187 <60312740+doob187@users.noreply.github.com>
2022-04-10 03:29:15 +0200

    Update install.sh

--- apps/docker-rollarr/root/rollarr/install.sh
+++ apps/docker-rollarr/root/rollarr/install.sh
@@ -117,20 +117,15 @@ PATH=/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
       pip install --no-warn-script-location --upgrade --force-reinstall -r /rollarr/requirements.txt
 
    mkdir -p /config \
-            /preroll \
+            /config/preroll \
            /config/preroll &>/dev/null
 
    useradd -u 911 -U -d /config -s /bin/false abc &>/dev/null && \
    usermod -G users abc &>/dev/null
 
-   ln -s /rollarr/data.json /config/data.json && \
-   ln -s /preroll /config/preroll
-
-   chmod -R 755 /rollarr/* \  
-            /config/data.json \
+   chmod -R 755 /rollarr/* \
             /crontab &>/dev/null
 
    chown -cR abc:abc /rollarr/* \
-         /config/data.json \
          /crontab &>/dev/null
 #EoF
