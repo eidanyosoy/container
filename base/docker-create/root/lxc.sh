9aa8183042525cc92648d835e37be698849d55d0
doob187 <60312740+doob187@users.noreply.github.com>
2022-04-11 20:13:22 +0200

    Create lxc.sh

---
+++ base/docker-create/root/lxc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p /opt/lxc
+
+cat > /opt/lxc/.lxcstart.sh << EOF; $(echo)
+#!/bin/bash
+#
+# Title:      LXC Bypass the mount :shared
+# OS Branch:  ubuntu,debian,rasbian
+# Author(s):  mrdoob
+# Coauthor:   DrAgOn141
+# GNU:        General Public License v3.0
+################################################################################
+## make / possible to add /mnt:shared
+mount --make-shared /
+EOF
