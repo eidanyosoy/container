ff9c6e69ada063840e45fad3b5ed2246d891fed1
doob187 <60312740+doob187@users.noreply.github.com>
2022-04-11 20:43:54 +0200

    Update lxc.sh

--- base/docker-create/root/lxc.sh
+++ base/docker-create/root/lxc.sh
@@ -1,4 +1,17 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bash
+# shellcheck shell=bash
+#####################################
+# All rights reserved.              #
+# started from Zero                 #
+# Docker owned dockserver           #
+# Docker Maintainer dockserver      #
+#####################################
+#####################################
+# THIS DOCKER IS UNDER LICENSE      #
+# NO CUSTOMIZING IS ALLOWED         #
+# NO REBRANDING IS ALLOWED          #
+# NO CODE MIRRORING IS ALLOWED      #
+#####################################
 
 mkdir -p /opt/lxc
 
