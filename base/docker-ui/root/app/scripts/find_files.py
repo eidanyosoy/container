5785572efb657ddf08fd0ebdfa3ef85d9d7bc18c
doob187 <60312740+doob187@users.noreply.github.com>
2022-04-16 11:01:32 +0200

    Update find_files.py

--- base/docker-ui/root/app/scripts/find_files.py
+++ base/docker-ui/root/app/scripts/find_files.py
@@ -5,20 +5,29 @@ find docker-compose.yml files
 import fnmatch
 import os
 import sys
+import glob
 import shutil
 import logging
 
 DOCKER_COMPOSE_UI_YML_PATH = '/opt/appdata/compose/'
+
+path = "/opt/appdata/compose"
+dirs = os.listdir( path )
 
 def find_yml_files(path):
     """
     find docker-compose.yml files in path
     """
     matches = {}
-    for root, _, filenames in os.walk(path, followlinks=True):
+
+    for root, dirs, filenames in os.walk(path, followlinks=True):
         for _ in set().union(fnmatch.filter(filenames, 'docker-compose.yml'), fnmatch.filter(filenames, 'docker-compose.yaml')):
             key = root.split('/')[-1]
-            matches[key] = os.path.join(os.getcwd(), root)
+            matches[key] = os.path.join(os.getcwd() root)
+       for name in files:
+           print(os.path.join(root, name))
+       for name in dirs:
+           print(os.path.join(root, name))
     return matches
 
 def get_readme_file(path):
@@ -27,7 +36,7 @@ def get_readme_file(path):
     """
     readme = None
 
-    for file in os.listdir(path):
+    for file in os.listdir( path ):
         if file.lower() == "readme.md" and os.path.isfile(os.path.join(path, file)):
             file = open(os.path.join(path, file))
             readme = file.read()
@@ -41,7 +50,7 @@ def get_logo_file(path):
     find case insensitive logo.png in path and return the contents
     """
     logo = None
-    for file in os.listdir(path):
+    for file in os.listdir( path ):
         if file.lower() == "logo.png" and os.path.isfile(os.path.join(path, file)):
             file = open(os.path.join(path, file))
             logo = file.read()
