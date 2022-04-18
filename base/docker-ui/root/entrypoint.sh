040617383a4e8f869885006a92fab614a67446f4
doob187 <60312740+doob187@users.noreply.github.com>
2022-04-18 11:51:17 +0200

    Update entrypoint.sh

--- base/docker-ui/root/entrypoint.sh
+++ base/docker-ui/root/entrypoint.sh
@@ -1,10 +1,23 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash                                                                           
 LFOLDER=/tmp
 
-if [ "$(ls -1p /opt/appdata/compose)" ];then
-   $(which git) clone git@github.com:dockserver/apps.git -C "${LFOLDER}/" --quiet
-   $(which rsync) "${LFOLDER}/" /opt/appdata/compose/ -aqhv
+if [[ "${GIT_REPO}" != "" ]];then
+   GIT_REPO=${GIT_REPO}
+   ## valide if exists
+   $(which git) ls-remote ${GIT_REPO} -q
+   if [[ $? != 0 ]];then
+      GIT_REPO=https://github.com/dockserver/apps.git
+   fi
+else
+   GIT_REPO=https://github.com/dockserver/apps.git
 fi
-
-/env/bin/gunicorn --chdir /app main:app -w 2 --threads 4 -b 0.0.0.0:5000
-
+              
+if [ "$(ls -1p /opt/appdata/compose)" ];then                                                  
+   $(which git) clone ${GIT_REPO} /tmp/apps --quiet
+   rm -rf /tmp/apps/.git /tmp/apps/upstream.sh /tmp/apps/README.md
+   $(which rsync) "${LFOLDER}/apps/" /opt/appdata/compose/ -aqhv
+   rm -rf /tmp/apps/*
+fi
+chown -cR 1000:1000 /opt/appdata/compose &>/dev/null
+                                                                                         
+/env/bin/gunicorn --chdir /app main:app -w 4 --threads 8 -b 0.0.0.0:5000
