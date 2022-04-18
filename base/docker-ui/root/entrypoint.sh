cb1a6a136513a6473f4c9fb37441caa29e2692e2
doob187 <60312740+doob187@users.noreply.github.com>
2022-04-18 11:31:27 +0200

    Update entrypoint.sh

--- base/docker-ui/root/entrypoint.sh
+++ base/docker-ui/root/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+LFOLDER=/tmp
+
+if [ "$(ls -1p /opt/appdata/compose)" ];then
+   $(which git) clone git@github.com:dockserver/apps.git -C "${LFOLDER}/" --quiet
+   $(which rsync) "${LFOLDER}/" /opt/appdata/compose/ -aqhv
+fi
+
 /env/bin/gunicorn --chdir /app main:app -w 2 --threads 4 -b 0.0.0.0:5000
 
