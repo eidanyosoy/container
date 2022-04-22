a8ed4d38627cbfe25a6dc116e04ab54617f66954
doob187 <60312740+doob187@users.noreply.github.com>
2022-04-22 13:08:49 +0200

    Update start.sh

--- base/docker-create/root/start.sh
+++ base/docker-create/root/start.sh
@@ -49,3 +49,155 @@ echo "**** install build packages ****" && \
     apk del --quiet --clean-protected --no-progress && \
     rm -f /var/cache/apk/*
 
+echo '{
+    "storage-driver": "overlay2",
+    "userland-proxy": false,
+    "dns": ["8.8.8.8", "1.1.1.1"],
+    "ipv6": false,
+    "log-driver": "json-file",
+    "live-restore": true,
+    "log-opts": {"max-size": "8m", "max-file": "2"}
+}' >/etc/docker/daemon.json
+
+## Check docker.sock
+DOCKER_HOST=/var/run/docker.sock
+if test -f "$DOCKER_HOST"; then
+   export DOCKER_HOST=$DOCKER_HOST
+else
+   export DOCKER_HOST='tcp://docker:2375'
+fi
+
+if docker compose > /dev/null 2>&1; then echo " stage 1 works " ; fi
+if docker help  > /dev/null 2>&1; then echo " stage 2 works " ; fi
+
+function domain() {
+printf "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+   🚀   Treafik Domain
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+     DNS
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+     DNS records will not be automatically added
+           with the following TLD Domains
+           .a, .cf, .ga, .gq, .ml or .tk
+     Cloudflare has limited their API so you
+          will have to manually add these
+   records yourself via the Cloudflare dashboard.
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+   read -erp "Which domain would you like to use?: " DOMAIN </dev/tty
+   if [[ $DOMAIN == "" ]]; then
+      echo "Domain cannot be empty" && domain
+   else
+      export $DOMAIN && traefik
+   fi
+
+}
+
+function displayname() {
+printf "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+   🚀   Authelia Username
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+   read -erp "Enter your username for Authelia (eg. John Doe): " DISPLAYNAME </dev/tty
+   if [[ $DISPLAYNAME == "" ]]; then
+      echo "Username cannot be empty" && displayname
+   else
+      export $DISPLAYNAME && traefik
+   fi
+}
+
+function password() {
+ printf "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+   🚀   Authelia Password
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+   read -erp "Enter a password for $USERNAME: " PASSWORD </dev/tty
+   if [[ $PASSWORD == "" ]]; then
+      echo "Password cannot be empty" && password
+   else
+      export $PASSWORD && traefik
+   fi
+}
+
+function cfemail() {
+printf "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+   🚀   Cloudflare Email-Address
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+   read -erp "What is your CloudFlare Email Address : " EMAIL </dev/tty
+   if [[ $EMAIL != "" ]]; then
+      export $EMAIL && traefik
+   else
+      echo "CloudFlare Email Address cannot be empty" && cfemail
+   fi
+}
+
+function cfkey() {
+printf "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+   🚀   Cloudflare Global-Key
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+   read -erp "What is your CloudFlare Global Key: " CFGLOBAL </dev/tty
+   if [[ $CFGLOBAL != "" ]]; then
+      export $CFGLOBAL && traefik
+   else
+      echo "CloudFlare Global-Key cannot be empty" && cfkey
+   fi
+}
+
+function cfzoneid() { 
+printf "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+   🚀   Cloudflare Zone-ID
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+   read -erp "Whats your CloudFlare Zone ID: " CFZONEID </dev/tty
+   if [[ $CFZONEID != "" ]]; then
+      export $CFZONEID && traefik
+   else
+      echo "CloudFlare Zone ID cannot be empty" && cfzoneid
+   fi
+}
+
+function deploynow() {
+
+## NOTE 
+## env schreiben ( basis env )
+## Authelia config schreiben
+## traefik compose live schreiben / oder nachrangiger compose in wget file
+## Authelia Password ? Doxker socket mounten? 
+## D-o-D system ? 
+## shell A Record hinzufügen bei CF ?
+
+echo " not done yet but should not token so long"
+echo " don't need 18 months , to get it working"
+
+}
+
+function traefik() {
+printf "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+   🚀   Treafik with Authelia over Cloudflare
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+   [1] Domain                         [ ${DOMAIN} ]
+   [2] Authelia Username              [ ${DISPLAYNAME} ]
+   [3] Authelia Password              [ ${PASSWORD} ]
+   [4] Cloudflare-Email-Address       [ ${CLOUDFLARE_EMAIL} ]
+   [5] Cloudflare-Global-Key          [ ${CLOUDFLARE_API_KEY} ]
+   [6] Cloudflare-Zone-ID             [ ${DOMAIN1_ZONE_ID} ]
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+   [D] Deploy Treafik with Authelia
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+   [ EXIT or Z ] - Exit
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+
+   read -erp '↘️  Type Number | Press [ENTER]: ' headtyped </dev/tty
+   case $headtyped in
+   1) domain ;;
+   2) displayname ;;
+   3) password ;;
+   4) cfemail ;;
+   5) cfkey ;;
+   6) cfzoneid ;;
+   d | D) deploynow ;;
+   Z | z | exit | EXIT | Exit | close) exit 0 ;;
+   *) clear && traefik ;;
+   esac
+}
+
+
+traefik
+
+# E-O-F FUU shitbox
