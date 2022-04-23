dfc7ac86eff5ec9555be3f68b293f06fb1cb03a0
doob187 <60312740+doob187@users.noreply.github.com>
2022-04-23 12:49:29 +0200

    Update start.sh

--- base/docker-create/root/start.sh
+++ base/docker-create/root/start.sh
@@ -32,6 +32,7 @@ echo "**** install build packages ****" && \
     shadow \
     musl \
     curl \
+    jq \
     findutils \
     coreutils \
     bind-tools \
@@ -90,8 +91,6 @@ fi
 function domain() {
 printf "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
    🚀   Treafik Domain
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-     DNS
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
      DNS records will not be automatically added
            with the following TLD Domains
@@ -101,12 +100,10 @@ printf "━━━━━━━━━━━━━━━━━━━━━━━━
    records yourself via the Cloudflare dashboard.
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
    read -erp "Which domain would you like to use?: " DOMAIN </dev/tty
-
-   echo $DOMAIN && sleep 20
-
-   if [ -z "$(dig +short "$DOMAIN")" ]; then
+   ##echo $DOMAIN && sleep 20
+   if [ ! -z "$(dig +short "$DOMAIN")" ]; then
       echo "$DOMAIN  is valid" && \
-        export $DOMAIN && \
+        export DOMAINNAME=$DOMAIN && \
           traefik
    else
       echo "Domain cannot be empty" && domain
@@ -123,7 +120,7 @@ printf "━━━━━━━━━━━━━━━━━━━━━━━━
       echo "Username cannot be empty" && \
         displayname
    else
-      export $AUTH_USERNAME && \
+      export AUTHUSERNAME=$AUTH_USERNAME && \
         traefik
    fi
 }
@@ -138,7 +135,7 @@ function password() {
       echo "Password cannot be empty" && \
         password
    else
-      export $AUTH_PASSWORD && \
+      export AUTHPASSWORD=$AUTH_PASSWORD && \
         traefik
    fi
 }
@@ -149,13 +146,11 @@ printf "━━━━━━━━━━━━━━━━━━━━━━━━
    🚀   Cloudflare Email-Address
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
    read -erp "What is your CloudFlare Email Address : " EMAIL </dev/tty
-
    regex="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$"
-
    if test -z "$EMAIL"; then
      if [[ $EMAIL =~ $regex ]] ; then
         echo "OK" && \
-          export $EMAIL && \
+          export CFEMAIL=$EMAIL && \
             traefik
      else
         echo "CloudFlare Email is not valid" && \
@@ -173,9 +168,8 @@ printf "━━━━━━━━━━━━━━━━━━━━━━━━
    🚀   Cloudflare Global-Key
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
    read -erp "What is your CloudFlare Global Key: " CFGLOBAL </dev/tty
-
    if test -z "$CFGLOBAL"; then
-      export $CFGLOBAL && traefik
+      export CFGLOBALKEY=$CFGLOBAL && traefik
    else
       echo "CloudFlare Global-Key cannot be empty" && cfkey
    fi
@@ -186,9 +180,8 @@ printf "━━━━━━━━━━━━━━━━━━━━━━━━
    🚀   Cloudflare Zone-ID
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
    read -erp "Whats your CloudFlare Zone ID: " CFZONEID </dev/tty
-
    if test -z "$CFZONEID"; then
-      export $CFZONEID && traefik
+      export CFZONEIDENT=$CFZONEID && traefik
    else
       echo "CloudFlare Zone ID cannot be empty" && cfzoneid
    fi
@@ -197,19 +190,11 @@ printf "━━━━━━━━━━━━━━━━━━━━━━━━
 
 function addcfrecord() {
 
-for i in curl jq bind-tools; do
-   apk add -U --update --no-cache $i &>/dev/null
-done
-
 ipv4=$($(which curl) -sX GET -4 https://ifconfig.co)
 ipv6=$($(which curl) -sX GET -6 https://ifconfig.co)
 checkipv4=$(dig @1.1.1.1 -4 ch txt whoami.cloudflare +short)
 checkipv6=$(dig @1.1.1.1 -6 ch txt whoami.cloudflare +short)
 
-## IPv4
-if [[ $checkipv4 == $ipv4 ]]; then
-   echo "$DOMAIN is currently set to $ipv4; no changes needed"
-else
    ## GET ZONE ID
    zoneid=$($(which curl) -sX GET "https://api.cloudflare.com/client/v4/zones?name=$EMAIL&status=active" \
        -H "X-Auth-Email: $EMAIL" \
@@ -222,50 +207,34 @@ else
        -H "X-Auth-Key: $CFGLOBAL" \
        -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
 
+## IPv4
    ## PUSH A RECORD FOR IPv4
    $(which curl) -sX PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsrecordid" \
        -H "X-Auth-Email: $EMAIL" \
        -H "X-Auth-Key: $CFGLOBAL" \
        -H "Content-Type: application/json" \
        --data "{\"type\":\"A\",\"name\":\"$DOMAIN\",\"content\":\"$ipv4\",\"ttl\":1,\"proxied\":true}" | jq
-fi
 
 ## IPv6
-if [[ $checkipv6 == $ipv6 ]]; then
-   echo "$DOMAIN is currently set to $ipv6; no changes needed"
-else
-   ## GET ZONE ID
-   zoneid=$($(which curl) -sX GET "https://api.cloudflare.com/client/v4/zones?name=$EMAIL&status=active" \
-       -H "X-Auth-Email: $EMAIL" \
-       -H "X-Auth-Key: $CFGLOBAL" \
-       -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
-
-   ## GET DNS RECORDID
-   dnsrecordid=$($(which curl) -sX GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=AAAA&name=$DOMAIN" \
-       -H "X-Auth-Email: $EMAIL" \
-       -H "X-Auth-Key: $CFGLOBAL" \
-       -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
-
    ## PUSH A RECORD FOR IPv6
    $(which curl) -sX PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsrecordid" \
        -H "X-Auth-Email: $EMAIL" \
        -H "X-Auth-Key: $CFGLOBAL" \
        -H "Content-Type: application/json" \
        --data "{\"type\":\"AAAA\",\"name\":\"$DOMAIN\",\"content\":\"$ipv6\",\"ttl\":auto,\"proxied\":true}" | jq
-fi
 }
 
 function deploynow() {
 
 ## NOTE 
-## env schreiben ( basis env ) ! done
-## Authelia config schreiben     || jinja kann das lösen 
-## traefik compose live schreiben / oder nachrangiger compose in wget file  || jinja-compose wird es lösen für uns
-## Authelia Password ? Docker socket mounten?   || tcpsocket or socket beides wird klappen
-## D-o-D system ?  || done
-## shell A Record hinzufügen bei CF ? || DONE
-## CLOUDFLARE TRUSTED IPS ändert sich immer wieder         {| done
-## Muss also gepullt werden und in Traefik geadded werden  {| done
+## env schreiben ( basis env )                                              {| done
+## Authelia config schreiben                                                {| jinja kann das lösen 
+## traefik compose live schreiben / oder nachrangiger compose in wget file  {| jinja-compose wird es lösen für uns
+## Authelia Password ? Docker socket mounten?                               {| tcpsocket or socket beides wird klappen
+## D-o-D system ?                                                           {| done
+## shell A Record hinzufügen bei CF ?                                       {| DONE
+## CLOUDFLARE TRUSTED IPS ändert sich immer wieder                          {| done
+## Muss also gepullt werden und in Traefik geadded werden                   {| done
 
 ## CF TRUSTED IPS LIVE PULL AND MAP
    if test -f "/tmp/trusted_cf_ips"; then $(which rm) -rf /tmp/trusted_cf_ips ; fi
@@ -275,29 +244,44 @@ function deploynow() {
    for i in `curl -sX GET "https://www.cloudflare.com/ips-v6"`; do echo $i >>/tmp/temp_trustedips ; done
 
    cat /tmp/temp_trustedips | while IFS=$'\n' read -ra CFTIPS; do echo -ne "${CFTIPS[0]}" >>/tmp/trusted_cf_ips ; done
-   if test -f "/tmp/endtrustedips";then $(which rm) -rf /tmp/endtrustedips ; fi
+     if test -f "/tmp/endtrustedips";then $(which rm) -rf /tmp/endtrustedips ; fi
 
 ## REMOVE LATEST , TO PREVENT IP FAILS
    cat /tmp/trusted_cf_ips | sed 's/.$//' >/tmp/endtrustedips
-   CFTRUSTEDIPS=$($(which cat) /tmp/endtrustedips)
+     CFTRUSTEDIPS=$($(which cat) /tmp/endtrustedips)
 
 ## SERVERIP 
-SERVERIP=$(curl -s http://whatismijnip.nl | cut -d " " -f 5)
+SERVERIP=$($(which curl) -s http://whatismijnip.nl | cut -d " " -f 5)
 if [[ "$SERVERIP" =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]; then
    echo "We found a valid IP | $SERVERIP | success"
 else
   echo "First test failed : Running secondary test now."
   if [[ $SERVERIP == "" ]];then
      echo "We found a valid IP | $SERVERIP | success" && \
-       SERVERIP=$(curl ifconfig.me) && \
-         export $SERVERIP
+       SERVERIP=$($(which curl) ifconfig.me) && \
+         export SERVERIP=$SERVERIP
   fi
 fi
+
+## VALUES FOR DEPLOY REPULL FROM EXPORT
+DOCKERHOST=$DOCKER_HOST
+DOMAINNAME=$DOMAIN
+AUTHUSERNAME=$AUTH_USERNAME
+AUTHPASSWORD=$AUTH_PASSWORD
+CFEMAIL=$EMAIL
+CFGLOBALKEY=$CFGLOBAL
+CFZONEIDENT=$CFZONEID
+SERVERIPGLOBAL=$SERVERIP
 
 ## AUTHELIA TOKENS
 JWTTOKEN=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
 SECTOKEN=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
 ENCTOKEN=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
+
+## ARGON PASSWORD HASHED
+$(which docker) pull authelia/authelia -q >/dev/null
+  AUTHPASSWORDSECRET=$($(which docker) run authelia/authelia authelia hash-password $AUTH_PASSWORD -i 2 -k 32 -m 128 -p 8 -l 32 | sed 's/Password hash: //g')
+    AUTHELIAPASSWORDSECRET=$(echo $AUTH_PASSWORD | sed -e 's/[\/&]/\\&/g')/g)
 
 ## ----
 
@@ -305,6 +289,10 @@ ENCTOKEN=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
 
 ## USER CONFIG 
 ## AUTHELIA CONFIG
+
+$(which docker) pull authelia/authelia -q >/dev/null
+  AUTHPASSWORDSECRET=$($(which docker) run authelia/authelia authelia hash-password $AUTH_PASSWORD -i 2 -k 32 -m 128 -p 8 -l 32 | sed 's/Password hash: //g')
+    AUTHELIAPASSWORDSECRET=$(echo $AUTH_PASSWORD | sed -e 's/[\/&]/\\&/g')/g)
 
 ## ----
 
@@ -319,25 +307,25 @@ CFTRUSTEDIPS=$($(which cat) /tmp/endtrustedips)
 
 echo -e "##Environment for Docker-Compose
 ## TRAEFIK
-CLOUDFLARE_EMAIL=${EMAIL}
-CLOUDFLARE_API_KEY=${CFGLOBAL}
+CLOUDFLARE_EMAIL=${CFEMAIL}
+CLOUDFLARE_API_KEY=${CFGLOBALKEY}
 CLOUDFLARE_TRUSTED_IPS=${CFTRUSTEDIPS}
-DOMAIN1_ZONE_ID=${CFZONEID}
-DOMAIN=${DOMAIN}
+DOMAIN1_ZONE_ID=${CFZONEIDENT}
+DOMAIN=${DOMAINNAME}
 CLOUDFLARED_UUID=${CLOUDFLARED_UUID:-TUNNEL_UUID_HERE}
 
 ## AUTHELIA 
-AUTHELIA_USERNAME=${AUTH_USERNAME}
-AUTHELIA_PASSWORD=${AUTH_PASSWORD}
+AUTHELIA_USERNAME=${AUTHUSERNAME}
+AUTHELIA_PASSWORD=${AUTHPASSWORD}
 
 ## TRAEFIK-ERROR-PAGES
 TEMPLATE_NAME=${TEMPLATE_NAME:-l7-dark}
 
 ## APPPART
-TZ=${TZ}
+TZ=${TZONE}
 ID=${ID:-1000}
 DOCKERNETWORK=${DOCKERNETWORK:-proxy}
-SERVERIP=${SERVERIP:-SERVERIP}
+SERVERIP=${SERVERIPGLOBAL}
 APPFOLDER=${APPFOLDER:-/opt/appdata}
 RESTARTAPP=${RESTARTAPP:-unless-stopped}
 UMASK=${UMASK:-022}
@@ -349,15 +337,15 @@ PLEX_CLAIM=${PLEX_CLAIM:-PLEX_CLAIM_ID}
 NS1=${NS1:-1.1.1.1}
 NS2=${NS2:-8.8.8.8}
 PORTBLOCK=${PORTBLOCK:-127.0.0.1}
-SOCKET=${SOCKET:-/var/run/docker.sock}
+SOCKET=${DOCKERHOST:-/var/run/docker.sock}
 SECURITYOPS=${SECURITYOPS:-no-new-privileges}
 SECURITYOPSSET=${SECURITYOPSSET:-true}
 ##EOF" >/opt/appdata/compose/.env
 
-## CLOUDFLARE A RECORD HIER !!!
-## ERLEICHTERT ALLES FÜR UNS
-## CF SETTINGS ?!
-## python oder doch bash ?!
+## CLOUDFLARE A RECORD HIER !!!  || DONE
+## ERLEICHTERT ALLES FÜR UNS     {| halb fertig
+## CF SETTINGS ?!                || muss python werden das bash limits hat
+## python oder doch bash ?!      {{ SIEHE LINE DRÜBER 
 
 echo " not done yet but should not token so long"
 echo " don't need 18 months , to get it working"
