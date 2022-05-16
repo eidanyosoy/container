dff034212e117a631257a39b71d197aaefedc77e
doob187 <60312740+doob187@users.noreply.github.com>
2022-05-16 19:15:51 +0200

    Update uploader.sh
    Add many notes 📝

--- apps/docker-uploader/root/app/uploader/uploader.sh
+++ apps/docker-uploader/root/app/uploader/uploader.sh
@@ -27,19 +27,19 @@ CHK=/system/uploader/logs/check.log
 EXCLUDE=/system/uploader/rclone.exclude
 CUSTOM=/app/custom
 
-## FOR MAPPING CLEANUP
+#### FOR MAPPING CLEANUP ####
 CONFIG=""
 CRYPTED=""
 BWLIMIT=""
 USERAGENT=""
 
+#### REMOVE LEFT OVER ####
 $(which mkdir) -p "${LOGFILE}" "${START}" "${DONE}" "${CUSTOM}"
 $(which find) "${BASE}" -type f -name '*.log' -delete
 $(which find) "${BASE}" -type f -name '*.txt' -delete
 $(which find) "${START}" -type f -name '*.json' -delete
 
-## EXPORT THE KEY SPECS
-
+#### EXPORT THE KEY SPECS ####
 if `ls -1p ${KEYLOCAL} | head -n1 | grep "GDSA" &>/dev/null`;then
     export KEY=GDSA
 elif `ls -1p ${KEYLOCAL} | head -n1 | grep -Po '\[.*?]' | sed 's/.*\[\([^]]*\)].*/\1/' | sed '/GDSA/d'`;then
@@ -48,8 +48,7 @@ else
     log "no match found of GDSA[01=~100] or [01=~100]" && sleep infinity
 fi
 
-### EXCLUDE PART
-
+#### EXCLUDE PART ####
 if [[ ! -f ${EXCLUDE} ]]; then
    cat > ${EXCLUDE} << EOF; $(echo)
 *-vpn/**
@@ -68,20 +67,15 @@ deluge/**
 EOF
 fi
 
-##### FUNCTIONS
-
+#### START OF ALL FUNCTIONS ####
 function cleanuplog() {
    RMLOG=/system/uploader/logs/rmcheck.log
    #### RCLONE LIST FILE
-   $(which rclone) lsf "${DONE}" \
-      --files-only -R \
-      --separator "|" \
-      --format="tp" \
-      --order-by="modtime" | sort > "${RMLOG}" 2>&1
+   $(which rclone) lsf "${DONE}" --files-only -R -s "|" -F "tp" | sort -n > "${RMLOG}" 2>&1
    #### REMOVE LAST 1000 FILES
    if [ `cat ${RMLOG} | wc -l` -gt 1000 ]; then
       $(which cat) "${RMLOG}" | head -n 1000 | while IFS=$'|' read -ra RMLO; do
-         $(which rm) -rf "${DONE}/${RMLO[1]}" "${CHK}" &>/dev/null
+         $(which rm) -rf "${DONE}/${RMLO[1]}" &>/dev/null
       done
    else
       $(which rm) -rf "${RMLOG}" "${CHK}" &>/dev/null
@@ -101,6 +95,7 @@ if test -f ${CSV} ; then
 
    $(which cat) ${CSV} | grep -E ${DIR} | sed '/^\s*#.*$/d'| while IFS=$'|' read -ra myArray; do
    if [[ ${myArray[2]} == "" && ${myArray[3]} == "" ]]; then
+### UNENCRYPTED RCLONE.CONF ####
 cat > ${ENDCONFIG} << EOF; $(echo)
 ## CUSTOM RCLONE.CONF for ${FILE}
 [${KEY}$[USED]]
@@ -110,9 +105,8 @@ server_side_across_configs = true
 service_account_file = ${JSONDIR}/${KEY}$[USED]
 team_drive = ${myArray[1]}
 EOF
-
    else
-
+#### CRYPTED CUSTOM RCLONE.CONF ####
 cat > ${ENDCONFIG} << EOF; $(echo)
 ## CUSTOM RCLONE.CONF
 [${KEY}$[USED]]
@@ -141,9 +135,8 @@ function rcloneupload() {
    MOVE=${MOVE:-/}
    FILE=$(basename "${UPP[1]}")
    DIR=$(dirname "${UPP[1]}" | sed "s#${DLFOLDER}/${MOVE}##g")
-   ####STARTZ=$(date +%s)
    SIZE=$(stat -c %s "${DLFOLDER}/${UPP[1]}" | numfmt --to=iec-i --suffix=B --padding=7)
-
+   #### CHECK IS FILE SIZE NOT CHANGE ####
    while true ; do
       SUMSTART=$(stat -c %s "${DLFOLDER}/${UPP[1]}")
       $(which sleep) 5
@@ -154,7 +147,7 @@ function rcloneupload() {
          $(which sleep) 10 ### longer sleeps for old drives
       fi
    done
-
+   #### CHECK IS CUSTOM RCLONE.CONF IS AVAILABLE ####
    if test -f "${CUSTOM}/${FILE}.conf" ; then
       CONFIG=${CUSTOM}/${FILE}.conf && \
         USED=`$(which rclone) listremotes --config=${CONFIG} | grep "$1" | sed -e 's/://g' | sed -e 's/GDSA//g' | sort`
@@ -163,54 +156,46 @@ function rcloneupload() {
         ARRAY=$($(which ls) -A ${KEYLOCAL} | wc -l ) && \
           USED=$(( $RANDOM % ${ARRAY} + 1 ))
    fi
-
    #### CRYPTED HACK ####
    if `$(which rclone) config show --config=${CONFIG} | grep ":/encrypt" &>/dev/null`;then
        export CRYPTED=C
    else
        export CRYPTED=""
    fi
-
+   #### TOUCH LOG FILE FOR UI READING ####
    touch "${LOGFILE}/${FILE}.txt" && \
       $(which echo) "{\"filedir\": \"${DIR}\",\"filebase\": \"${FILE}\",\"filesize\": \"${SIZE}\",\"logfile\": \"${LOGFILE}/${FILE}.txt\",\"gdsa\": \"${KEY}$[USED]${CRYPTED}\"}" > "${START}/${FILE}.json"
-
    #### READ BWLIMIT ####
    if [[ "${BANDWITHLIMIT}" =~ ^[0-9][0-9]+([.][0-9]+)?$ ]]; then
       BWLIMIT="--bwlimit=${BANDWITHLIMIT}"
    fi
-
    if [[ "${TRANSFERS}" != 1 ]];then
       $(which sleep) 2 ## sleep 5 for duplicati folders
       ### make folder on correct drive
       $(which rclone) mkdir "${KEY}$[USED]${CRYPTED}:/${DIR}/" --config="${CONFIG}"
    fi
-
    #### START TIME UPLOAD ####
    STARTZ=$(date +%s)
-
-   #### RUN MOVE ####
+   #### RUN RCLONE UPLOAD COMMAND ####
    $(which rclone) moveto "${DLFOLDER}/${UPP[1]}" "${KEY}$[USED]${CRYPTED}:/${DIR}/${FILE}" \
       --config="${CONFIG}" \
-      --stats=1s \
-      --checkers=2 \
+      --stats=1s --checkers=2 \
       --drive-chunk-size=8M \
       --log-level="${LOG_LEVEL}" \
       --user-agent="${USERAGENT}" ${BWLIMIT} \
       --log-file="${LOGFILE}/${FILE}.txt" \
       --tpslimit 20
- 
-   ## END TIME UPLOAD ##
+   #### END TIME UPLOAD ####
    ENDZ=$(date +%s)
-
+   #### ECHO END-PARTS FOR UI READING ####
    $(which find) "${DLFOLDER}/${SETDIR}" -type d -empty -delete &>/dev/null
-
    $(which echo) "{\"filedir\": \"${DIR}\",\"filebase\": \"${FILE}\",\"filesize\": \"${SIZE}\",\"gdsa\": \"${KEY}$[USED]${CRYPTED}\",\"starttime\": \"${STARTZ}\",\"endtime\": \"${ENDZ}\"}" > "${DONE}/${FILE}.json"
+   #### UNSET CRYPTED WHEN USED CRYPTED KEYS ####
    unset CRYPTED
-   #### END OF MOVE
-
+   #### END OF MOVE ####
    $(which rm) -rf "${LOGFILE}/${FILE}.txt" "${START}/${FILE}.json" 
    $(which chmod) 755 "${DONE}/${FILE}.json"
-
+   #### REMOVE CUSTOM RCLONE.CONF ####
    if test -f "${CUSTOM}/${FILE}.conf";then
       $(which rm) -rf ${CUSTOM}/${FILE}.conf
    fi
@@ -219,18 +204,14 @@ function rcloneupload() {
 function listfiles() {
    source /system/uploader/uploader.env
    DLFOLDER=${DLFOLDER}
-   #### RCLONE LIST FILE
-   $(which rclone) lsf "${DLFOLDER}" \
-      --files-only -R \
-      --separator "|" \
-      --format="tp" \
-      --order-by="modtime" \
-      --exclude-from="${EXCLUDE}" | sort > "${CHK}" 2>&1
+   #### RCLONE LIST FILE ####
+   $(which rclone) lsf "${DLFOLDER}" --files-only -R -s "|" -F "tp" --exclude-from="${EXCLUDE}" | sort -n > "${CHK}" 2>&1
 }
 
 function checkspace() {
    source /system/uploader/uploader.env
    DLFOLDER=${DLFOLDER}
+   #### CHECK DRIVEUSEDSPACE ####
    if [[ "${DRIVEUSEDSPACE}" =~ ^[0-9][0-9]+([.][0-9]+)?$ ]]; then
       while true ; do
         LCT=$($(which df) --output=pcent ${DLFOLDER} | tr -dc '0-9')
@@ -248,19 +229,16 @@ function checkspace() {
 function transfercheck() {
    while true ; do
        source /system/uploader/uploader.env
-       #### -I [ exclude check.log & rmcheck.log file ]
-       #### moved to pgrep
-       #### ACTIVETRANSFERS=`$(which pgrep) -x rclone | wc -l`
+       #### -I [ exclude check.log & rmcheck.log file ] ####
        ACTIVETRANSFERS=`ls -A ${LOGFILE} -I "check.log" -I "rmcheck.log" | wc -l`
        TRANSFERS=${TRANSFERS:-2}
          if [[ ${ACTIVETRANSFERS} -lt ${TRANSFERS} ]]; then
             #### REMOVE ACTIVE UPLOAD from check file
             $(which touch) "${LOGFILE}/${FILE}.txt"
-            #### change modtime of file
+            #### CHANGE MODTIME OF FILE ####
             $(which touch) -m "${DLFOLDER}/${UPP[1]}"
-            #### reload check file
+            #### RELOAD CHECK FILE ####
             listfiles
-            #### $(which sed) -i -e '1 w /dev/stdout' -e '1d' "${CHK}" &>/dev/null   ## to prevent double upload trying
             $(which sleep) 5 && break
          else
             $(which sleep) 10
@@ -270,7 +248,7 @@ function transfercheck() {
 
 function rclonedown() {
    source /system/uploader/uploader.env
-   ## upload function shutdown
+   #### SHUTDOWN UPLOAD LOOP WHEN DRIVE SPACE IS LESS THEN SETTINGS ####
    LCT=$($(which df) --output=pcent ${DLFOLDER} | tr -dc '0-9')
    if [[ "${DRIVEUSEDSPACE}" =~ ^[0-9][0-9]+([.][0-9]+)?$ ]]; then
       if [[ "${DRIVEUSEDSPACE}" -gt "${LCT}" ]]; then
@@ -283,21 +261,21 @@ function rclonedown() {
 
 #### START HERE UPLOADER LIVE
 while true ; do
-   #### run check space
+   #### RUN CHECK SPACE ####
    checkspace
-   #### run list as function 
+   #### RUN LIST COMMAND-FUNCTION ####
    listfiles
    #### FIRST LOOP
    if [ `$(which cat) ${CHK} | wc -l` -gt 0 ]; then
       # shellcheck disable=SC2086
       $(which cat) "${CHK}" | head -n 1 | while IFS=$'|' read -ra UPP; do
-         #### run transfers check
+         #### RUN TRANSFERS CHECK ####
          transfercheck
-         #### Looping to correct drive
+         #### SET CORRECT FOLDER FOR CUSTOM UPLOAD RCLONE.CONF ####
          SETDIR=$(dirname "${UPP[1]}" | sed "s#${DLFOLDER}/${MOVE}##g" | cut -d ' ' -f 1 | sed 's|/.*||' )
-         #### checking multiple drive setup
+         #### CHECK IS CSV AVAILABLE AND LOOP TO CORRECT DRIVE ####
          if test -f ${CSV}; then loopcsv ; fi
-         #### upload function startup
+         #### UPLOAD FUNCTIONS STARTUP ####
          if [[ "${TRANSFERS}" != 1 ]];then
             #### DEMONISED UPLOAD
             rcloneupload &
@@ -305,13 +283,15 @@ while true ; do
             #### SINGLE UPLOAD
             rcloneupload
          fi
-         #### shutdown rclone upload process
+         #### SHUTDOWN RCLONE UPLOAD PROCESS ####
          rclonedown
       done
+      #### CLEANUP OLD JSON FILES WHEN OVER 1000 FILES ###
       cleanuplog
    else
+      #### SLEEP REDUCES CPU AND RAM USED ####
       sleep 120
    fi
 done
 
-## END OF FILE
+#### END OF FILE ####
