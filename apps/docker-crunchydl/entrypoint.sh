0761603a675626b17fe90db3f4db7e2fd1ef2a06
doob187 <60312740+doob187@users.noreply.github.com>
2022-06-06 14:37:40 +0200

    Update entrypoint.sh

--- apps/docker-crunchydl/entrypoint.sh
+++ apps/docker-crunchydl/entrypoint.sh
@@ -29,7 +29,9 @@ $(which echo) "**** install packages ****" && \
 
 ### READ TO DOWNLOAD FILE ###
 CHK=/config/download.txt
-### FINAL FOLDER ###
+LOCHK=/config/to-download.txt
+
+## RUN LOOP ##
 
 while true ; do
   CHECK=$($(which cat) ${CHK} | wc -l)
@@ -37,11 +39,34 @@ while true ; do
      ### READ FROM FILE AND PARSE ###
      $(which cat) "${CHK}" | head -n 1 | while IFS=$'|' read -ra SHOWLINK ; do
         $(which echo) "**** downloading now ${SHOWLINK[1]} ****"
-       ./aniDL --username ${EMAIL} --password ${PASSWORD} --new \
-       --service ${SHOWLINK[0]} --series ${SHOWLINK[1]} \
-       -q 0 --dlsubs all --dubLang ${SHOWLINK[2]} \
-       --filename=${showTitle}.${title}.S${season}E${episode}.WEBHD.${height} \
-       --force Y --mp4 --nocleanup --skipUpdate
+          ./aniDL \
+          --username ${EMAIL} --password ${PASSWORD} \
+          --new \
+          --service ${SHOWLINK[0]} \
+          --series ${SHOWLINK[2]} \
+          -q 0 \
+          --dlsubs all \
+          --dubLang ${SHOWLINK[1]} \
+          --filename=${showTitle}.${title}.S${season}E${episode}.WEBHD.${height} \
+          --force Y \
+          --mp4 \
+          --nocleanup \
+          --skipUpdate
+          $(which cat) "${CHK}" | awk 'NR==1; END{print}' >> "${LOCHK}"
+          $(which sed) -i 1d "${CHK}"
+          ## RUN FFMPEG TO COVENT TO MKV ###
+          shopt -s globstar
+          for f in /videos/**/*.mkv; do
+            ## c:v/s/a >> video _ subtitle _ audio  >> copy from mp4
+            $(which echo) "**** running  convert for ${f} ****" && \
+              $(which ffmpeg) -nostdin -i "$f" -c:v copy -c:a copy -c:s copy "${f%.mp4}-dockserver.mkv" && \
+              $(which chown) -cR 1000:1000 "$f" &>/dev/null && \
+            $(which rm) -rf "{$f}.mp4" &>/dev/null 
+          done
+          CHECK=$($(which cat) ${CHK} | wc -l)
+       if [ "${CHECK}" == 0 ]; then
+          $(which mv) "${LOCHK}" "${CHK}"
+       fi
   else
       $(which echo) "**** nothing to download yet ****" && \
          $(which sleep) 240
