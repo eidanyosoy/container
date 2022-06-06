ae4a68feb2a2fac678faa1e75a7059a1dbf213aa
doob187 <60312740+doob187@users.noreply.github.com>
2022-06-06 17:54:50 +0200

    Update entrypoint.sh

--- apps/docker-crunchydl/entrypoint.sh
+++ apps/docker-crunchydl/entrypoint.sh
@@ -37,36 +37,46 @@ while true ; do
   CHECK=$($(which cat) ${CHK} | wc -l)
   if [ "${CHECK}" -gt 0 ]; then
      ### READ FROM FILE AND PARSE ###
-     $(which cat) "${CHK}" | head -n 1 | while IFS=$'|' read -ra SHOWLINK ; do
-        $(which echo) "**** downloading now ${SHOWLINK[1]} ****"
-          ./aniDL \
-          --username ${EMAIL} --password ${PASSWORD} \
-          --new \
-          --service ${SHOWLINK[0]} \
-          --series ${SHOWLINK[2]} \
-          -q 0 \
-          --dlsubs all \
-          --dubLang ${SHOWLINK[1]} \
-          --filename=${showTitle}.${title}.S${season}E${episode}.WEBHD.${height} \
-          --force Y \
-          --mp4 \
-          --nocleanup \
-          --skipUpdate
+       $(which cat) "${CHK}" | head -n 1 | while IFS=$'|' read -ra SHOWLINK ; do
+          if [[ "${SHOWLINK[0]}" == tv ]]; then
+              $(which echo) "**** downloading now ${SHOWLINK[1]} ****"
+              ./aniDL \
+              --username ${EMAIL} --password ${PASSWORD} --new \
+              --service ${SERVICE} --series ${SHOWLINK[1]} \
+              --videoTitle ${title} --dubLang ${DUBLANG} \
+              --filename=${SHOWLINK[0]}/${showTitle}/${showTitle}.${title}.S${season}E${episode}.WEBHD.${height} \
+              --force Y --mp4 --nocleanup --skipUpdate
+          elif [[ "${SHOWLINK[0]}" == movie ]]; then
+              $(which echo) "**** downloading now ${SHOWLINK[1]} ****"
+              ./aniDL \
+              --username ${EMAIL} --password ${PASSWORD} --new \
+              --service ${SERVICE} --movie-listing ${SHOWLINK[1]} \
+              --videoTitle ${title} --dubLang ${DUBLANG} \
+              --filename=${SHOWLINK[0]}/${title}/${showTitle}.${title}.WEBHD.${height} \
+              --force Y --mp4 --nocleanup --skipUpdate
+          else
+              $(which echo) "**** could not terminate what you want to load ...... atsch .... ****" 
+          fi
+          shopt -s globstar
+          for f in /videos/${SHOWLINK[0]}/**/*.mp4; do
+             $(which mv) "$f" "${f// /.}" &>/dev/null
+          done
           $(which cat) "${CHK}" | awk 'NR==1; END{print}' >> "${LOCHK}"
           $(which sed) -i 1d "${CHK}"
           ## RUN FFMPEG TO COVENT TO MKV ###
           shopt -s globstar
-          for f in /videos/**/*.mkv; do
-            ## c:v/s/a >> video _ subtitle _ audio  >> copy from mp4
-            $(which echo) "**** running  convert for ${f} ****" && \
-              $(which ffmpeg) -nostdin -i "$f" -c:v copy -c:a copy -c:s copy "${f%.mp4}-dockserver.mkv" && \
-              $(which chown) -cR 1000:1000 "$f" &>/dev/null && \
-            $(which rm) -rf "{$f}.mp4" &>/dev/null 
+          for f in /videos/**/*.mp4; do
+             ## c:v/s/a >> video _ subtitle _ audio  >> copy from mp4
+             $(which echo) "**** running  convert for ${f} ****" && \
+             $(which ffmpeg) -nostdin -i "$f" -c:v copy -c:a copy -c:s copy "${f%.mp4}-dockserver.mkv" && \
+             $(which chown) -cR 1000:1000 "$f" &>/dev/null && \
+             $(which rm) -rf "{$f}.mp4" &>/dev/null 
           done
           CHECK=$($(which cat) ${CHK} | wc -l)
-       if [ "${CHECK}" == 0 ]; then
-          $(which mv) "${LOCHK}" "${CHK}"
-       fi
+          if [ "${CHECK}" == 0 ]; then
+             $(which mv) "${LOCHK}" "${CHK}"
+          fi
+       done
   else
       $(which echo) "**** nothing to download yet ****" && \
          $(which sleep) 240
