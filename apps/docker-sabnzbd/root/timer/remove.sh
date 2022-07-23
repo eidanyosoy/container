3e643fca2e337e57474dd020b2e4d891d3957cb9
doob187 <60312740+doob187@users.noreply.github.com>
2022-07-23 20:19:48 +0200

    Create remove.sh (#62)
    * Create remove.sh
    
    * Create run
    
    * Update remove.sh
    
    Add  chown part ..
    For possible  miss configs

---
+++ apps/docker-sabnzbd/root/timer/remove.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+####################################
+# All rights reserved.              #
+# started from Zero                 #
+# Docker owned dockserver           #
+# Docker Maintainer dockserver      #
+#####################################
+#####################################
+# THIS DOCKER IS UNDER LICENSE      #
+# NO CUSTOMIZING IS ALLOWED         #
+# NO REBRANDING IS ALLOWED          #
+# NO CODE MIRRORING IS ALLOWED      #
+#####################################
+# shellcheck disable=SC2086
+# shellcheck disable=SC2046
+while true; do
+   for removenzbs in `cat /app/sabnzbd/sabnzbd.ini | grep "nzb_backup_dir" | cut -d. -f2 |awk '{print $3}'` ; do
+      find $removenzbs -type f -name "*nzb*" -exec rm -rf \;
+   done
+   sleep 30m
+   for removefailed in `cat /app/sabnzbd/sabnzbd.ini | grep "complete_dir" | cut -d. -f2 |awk '{print $3}'` ; do
+      find $removefailed -type d -name "*_FAILED*" -exec rm -rf \;
+   done
+   sleep 30m
+   for setperms in `cat /app/sabnzbd/sabnzbd.ini | grep "complete_dir" | cut -d. -f2 |awk '{print $3}'` ; do
+      chown -cR 1000:1000 $setperms &>/dev/null
+   done
+   sleep 30m
+done
