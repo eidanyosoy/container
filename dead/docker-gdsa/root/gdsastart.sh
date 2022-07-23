8e1ba916ed9b5ead8fb22c5b36c3db9bafdbe8de
doob187 <60312740+doob187@users.noreply.github.com>
2022-07-23 17:33:47 +0200

    Update gdsastart.sh

--- dead/docker-gdsa/root/gdsastart.sh
+++ dead/docker-gdsa/root/gdsastart.sh
@@ -21,6 +21,7 @@ setorg=$(gcloud organizations list --format="value(ID)")
 export ORGANIZATIONID=$setorg
 sleep $CYCLEDELAY
 }
+
 create_projects() {
 while
   : ${start=$i}
@@ -29,12 +30,14 @@ while
 do :; done
 sleep $CYCLEDELAY
 }
+
 enable_apis() {
     gcloud config set project $PROJECT
     gcloud services enable drive.googleapis.com sheets.googleapis.com \
         admin.googleapis.com cloudresourcemanager.googleapis.com servicemanagement.googleapis.com
 sleep $CYCLEDELAY
 }
+
 create_gdsas() {
 gcloud config set project $PROJECT
 let LAST_SA_NUM=$COUNT+$NUMGDSAS-1
@@ -47,6 +50,7 @@ sleep $CYCLEDELAY
 SA_COUNT=`gcloud iam service-accounts list --format="value(EMAIL)" | sort | wc -l`
 let COUNT=$COUNT+$NUMGDSAS
 }
+
 create_keys() {
     gcloud config set project $PROJECT
 TOTAL_JSONS_BEF=`ls $KEYSDONE | grep "GDSA" | wc -l`
@@ -62,6 +66,7 @@ TOTAL_JSONS_NOW=`ls $KEYSDONE | grep "GDSA" | wc -l`
 let TOTAL_JSONS_MADE=$TOTAL_JSONS_NOW-$TOTAL_JSONS_BEF
 let COUNT=$COUNT+$NUMGDSAS
 }
+
 create_config() {
 TOTAL_JSONS_BEF=`ls $KEYSDONE | grep "GDSA" | wc -l`
 for build in $(seq $COUNT $TOTAL_JSONS_BEF); do
@@ -89,11 +94,11 @@ password2 = $SALT
 fi
 done
 }
+
 makecsv() {
-set -x
   gcloud iam service-accounts list --project ${PROJECT} --format="value(email)" | sort > /system/servicekeys/members.csv
-set +x
 }
+
 main() {
 for function in exportorg create_projects enable_apis create_gdsas create_keys create_config makecsv;do
     COUNT=$FIRSTGDSA
@@ -117,18 +122,10 @@ if [[ -f "/system/servicekeys/rclonegdsa.conf" ]];then $(which rm) -rf /system/s
 
 
 if [[ -f "/system/servicekeys/.env" ]]; then 
-   rm -rf "/system/servicekeys/.env" 
+   $(which rm) -rf "/system/servicekeys/.env" 
 fi
 
-
-### GENERATE  READ LAYOUT IN DOCKER ####
-
-
-
-### ECHO VALUES OR USE EXPORT  
-### EXPORT WILL BE EASIER TO RERUN IT 
-### AND DONT NEED ECHO TO FILE COMMAND 
-
+## READ FROM EXPORTED ENVS
 CYCLEDELAY=0.1s
 SANAME=${SANAME}
 FIRSTGDSA=1
@@ -142,46 +139,16 @@ export ACCOUNT=${ACCOUNT}
 export PROJECT=${PROJECT}
 export TEAMDRIVEID=${TEAMDRIVEID}
 
-
+## ENCRYPTED
 export ENCRYPT=${ENCRYPT}
 export PASSWORD=${PASSWORD}
 export SALT=${SALT}
 
-
-## FROM HERE RUN IT TO GENERATE IT
-### REREAD THE SOURCE FILE 
-
-## OR READ ABOVE 
-
-### END COMMAND IS 
-
-### WILL GENERATE ALL THE KEYS
 main
-
-
-
-
-CYCLEDELAY=0.1s
-SANAME=${SANAME}
-FIRSTGDSA=1
-LASTPROJECTNUM=1
-export NUMGDSAS=${NUMGDSAS}
-export PROGNAME=${PROGNAME}
-SECTION_DELAY=2
-#### USER VALUES ####
-export ACCOUNT=${ACCOUNT}
-export PROJECT=${PROJECT}
-ORGANIZATIONID=${ORGANIZATIONID}
-export TEAMDRIVEID=${TEAMDRIVEID}
-export ENCRYPT=${ENCRYPT}
-export PASSWORD=${PASSWORD}
-export SALT=${SALT}
-
-
 
 ## TO RUN IT 
 ## EASIER IS NOT POSSIBLE 
-export alias gdsagen='docker run -it -v /opt/appdata/system/servicekeys/:/system/servicekeys/ ghcr.io/dockserver/docker-gdsa:latest /bin/bash /gdsastart.sh'
-source ~/.bashrc
+##export alias gdsagen='docker run -it -v /opt/appdata/system/servicekeys/:/system/servicekeys/ ghcr.io/dockserver/docker-gdsa:latest /bin/bash /gdsastart.sh'
+##source ~/.bashrc
 
-
+#/E_o_F\#
