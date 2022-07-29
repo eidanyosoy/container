a5c799fa38f38919afae53f1eea3bece17141acb
doob187 <60312740+doob187@users.noreply.github.com>
2022-07-29 17:53:19 +0200

    Update remove.sh (#68)
    * Update remove.sh
    
    * Create 30-addon

--- apps/docker-sabnzbd/root/timer/remove.sh
+++ apps/docker-sabnzbd/root/timer/remove.sh
@@ -14,15 +14,15 @@
 # shellcheck disable=SC2086
 # shellcheck disable=SC2046
 while true; do
-   for removenzbs in `cat /app/sabnzbd/sabnzbd.ini | grep "nzb_backup_dir" | cut -d. -f2 |awk '{print $3}'` ; do
+   for removenzbs in `cat /config/sabnzbd.ini | grep "nzb_backup_dir" | cut -d. -f2 |awk '{print $3}'` ; do
       find $removenzbs -type f -name "*nzb*" -exec rm -rf \;
    done
    sleep 30m
-   for removefailed in `cat /app/sabnzbd/sabnzbd.ini | grep "complete_dir" | cut -d. -f2 |awk '{print $3}'` ; do
+   for removefailed in `cat /config/sabnzbd.ini | grep "complete_dir" | cut -d. -f2 |awk '{print $3}'` ; do
       find $removefailed -type d -name "*_FAILED*" -exec rm -rf \;
    done
    sleep 30m
-   for setperms in `cat /app/sabnzbd/sabnzbd.ini | grep "complete_dir" | cut -d. -f2 |awk '{print $3}'` ; do
+   for setperms in `cat /config/sabnzbd.ini | grep "complete_dir" | cut -d. -f2 |awk '{print $3}'` ; do
       chown -cR 1000:1000 $setperms &>/dev/null
    done
    sleep 30m
