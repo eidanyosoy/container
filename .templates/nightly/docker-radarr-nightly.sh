f1e633cd2226e1226787b6cb6e13084ab216553b
doob187 <60312740+doob187@users.noreply.github.com>
2022-08-26 10:42:57 +0200

    Update docker-radarr-nightly.sh

--- .templates/nightly/docker-radarr-nightly.sh
+++ .templates/nightly/docker-radarr-nightly.sh
@@ -30,7 +30,7 @@ fi
 
 APPBRANCH="nightly"
 APPLINK="https://api.github.com/repos/radarr/radarr"
-NEWVERSION=$(curl -u $USERNAME:TOKEN -sX GET https://api.github.com/repos/linuxserver/docker-radarr/releases | jq -r 'first(.[] | select(.target_commitish | contains("nightly") )) | .tag_name' 
+NEWVERSION=$(curl -u $USERNAME:TOKEN -sX GET https://api.github.com/repos/linuxserver/docker-radarr/releases | jq -r 'first(.[] | select(.target_commitish | contains("nightly") )) | .tag_name' )
 
 #####select(.prerelease==true )) | .tag_name')
 
