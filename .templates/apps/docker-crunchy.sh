8185cee668f51832b5bc4902f9bd0bab05a08fb9
DrAg0n141 <44865095+drag0n141@users.noreply.github.com>
2022-10-25 09:41:54 +0200

    Update docker-crunchy.sh

--- .templates/apps/docker-crunchy.sh
+++ .templates/apps/docker-crunchy.sh
@@ -22,7 +22,7 @@ TOKEN=$4
 ### APP SETTINGS ###
 APPLINK="https://api.github.com/repos/dockserver/dockserver"
 BUILDVERSION=$(curl -sX GET "https://registry.hub.docker.com/v2/repositories/library/ubuntu/tags" \
-   | jq --raw-output '.[] | select(.name | contains(".")) | .name' \
+   | jq -r 'select(.results != null) | .results[]["name"]'' \
    | sort -t "." -k1,1n -k2,2n -k3,3n | tail -n1)
 BUILDVERSION="${BUILDVERSION#*v}"
 BUILDVERSION="${BUILDVERSION#*release-}"
